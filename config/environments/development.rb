f7d726fa2a1b89c0916e95dc140441531bf545ea
JuanSAuzaO <sebastianauza@hotmail.com>
2023-01-18 14:20:16 -0500

    Setup email functionality in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :test
+
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000'}
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
